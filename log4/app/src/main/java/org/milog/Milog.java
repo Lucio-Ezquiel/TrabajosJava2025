d73cee603c6ad392a8d04a74395793046101b7aa
Tiago CB <16941297+YankeeDeMierda@users.noreply.github.com>
2025-05-23 17:48:01 -0300

    Fin.

--- log4/app/src/main/java/org/milog/Milog.java
+++ log4/app/src/main/java/org/milog/Milog.java
@@ -3,6 +3,7 @@ package org.milog;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
 
 /**
  * Milog
@@ -22,5 +23,16 @@ public class Milog {
         logger.warn("New file log msg");
         // logger.fatal("Http funca con python flask"); // tiro mucho error pero
         // funciono
+        logger.fatal("Hola log msg");
+
+        // Agregar información al contexto del hilo
+        ThreadContext.put("userId", "12345");
+        ThreadContext.put("transactionId", "TXN789");
+
+        // Generar un mensaje de logging
+        logger.info("Procesando transacción para el usuario.");
+
+        // Limpiar el contexto del hilo
+        ThreadContext.clearAll();
     }
 }
